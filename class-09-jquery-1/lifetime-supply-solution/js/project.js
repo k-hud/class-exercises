02b0a37143bb464eda6a0b8d86bd26ebb05304c3
glumac <mglumac@gmail.com>
2015-11-24 18:05:47 -0500

    semicolons

--- class-09-jquery-1/lifetime-supply-solution/js/project.js
+++ class-09-jquery-1/lifetime-supply-solution/js/project.js
@@ -1,17 +1,17 @@
 // assign click event handler
-document.getElementById('clickme').onclick = calculateNumDrinks
+document.getElementById('clickme').onclick = calculateNumDrinks;
 
 function calculateNumDrinks () {
 	// assign variables
-	var currentAge = document.getElementById('age').value
-	var maxAge = document.getElementById('maxAge').value
-	var favDrink = document.getElementById('item').value
-	var numPerDay = document.getElementById('numPerDay').value
+	var currentAge = document.getElementById('age').value;
+	var maxAge = document.getElementById('maxAge').value;
+	var favDrink = document.getElementById('item').value;
+	var numPerDay = document.getElementById('numPerDay').value;
 
 	// calculate total
-	var total = (maxAge - currentAge) * 365 * numPerDay
+	var total = (maxAge - currentAge) * 365 * numPerDay;
 
 	// output results
-	document.getElementById('solution').innerHTML = total
-	document.getElementById('drink').innerHTML = favDrink
+	document.getElementById('solution').innerHTML = total;
+	document.getElementById('drink').innerHTML = favDrink;
 }
